5bb5831d94372740ec7a821f8160c985afcdbfe4
Han Kim <bagsangcheol@HanKyulKim-MacBook-Air.local>
2019-08-16 14:52:38 -0700

    schema added (rank by size), potentially fixed Search Component

--- database/index.js
+++ database/index.js
@@ -1,16 +1,35 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+//get notified if connection is successful
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected!');
+})
+
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  name: String,
+  html_url: String,
+  size: Number,
+  language: String
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (function(err, data) {
+  if (err) {
+    return console.error(err);
+  } else {
+    console.log("saved into database");
+  }
+};
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+  //use insertMany if data is array of repos with information
 }
 
 module.exports.save = save;
